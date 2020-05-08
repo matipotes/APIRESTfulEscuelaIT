7d6467e61e63a82988aac1050848d516c0d1cc04
matipotes <matipotes@gmail.com>
2020-05-08 10:39:47 -0300

    Clase2 se crearon las columnas de las tablas y claves foraneas

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'seller_id',
+
+    ];
 }
--- database/migrations/2020_05_06_135253_create_category_product_table.php
+++ database/migrations/2020_05_06_135253_create_category_product_table.php
@@ -14,8 +14,12 @@ class CreateCategoryProductTable extends Migration
     public function up()
     {
         Schema::create('category_product', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('category_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('product_id')->references('id')->on('products');
+            
         });
     }
 
