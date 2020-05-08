dc56d0e97a8dea11cb1b8a498b544cb8b45becc1
matipotes <matipotes@gmail.com>
2020-05-08 12:09:40 -0300

    Se ejecutaron las migraciones con fresh

--- database/migrations/2020_05_05_215313_create_products_table.php
+++ database/migrations/2020_05_05_215313_create_products_table.php
@@ -14,12 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('description', 1000);
-            $table->integer('quantity')->unsigned();
-            $table->string('status');
-            $table->string('seller_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('status');
+            $table->integer('seller_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('seller_id')->references('id')->on('users');
