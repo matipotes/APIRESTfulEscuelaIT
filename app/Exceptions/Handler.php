035c219be42fd670ddc665a3f4cd6fbefb43bbc7
mpotesv <mpotes@vaclog.com>
2020-05-18 16:33:57 -0300

    Se restauraron las excepciones de la clase3: Generalizando el Código y Mejorando el Funcionamiento de la API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,14 +2,22 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
-     *
+     * Podemos especificar el tipo de excepciones que no quisieramos que quedaran en el archivo laravel.log
      * @var array
      */
     protected $dontReport = [
@@ -28,7 +36,7 @@ class Handler extends ExceptionHandler
 
     /**
      * Report or log an exception.
-     *
+     * Se encarga de loguear las excepciones en el archivo laravel.log
      * @param  \Throwable  $exception
      * @return void
      *
@@ -50,6 +58,56 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            $modelName = class_basename($exception->getModel());
+            return $this->errorResponse("Does not exists any instance of {$modelName} with the specified id", 404);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('Does not exist any endpoint matching with that URL', 404);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse(
+                'Does not exist any endpoint matching with that URL',
+                $exception->getStatusCode()
+            );
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse(
+                'HTTP method does not match with any endpoint',
+                $exception->getStatusCode()
+            );
+        }
+
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected error', 500);
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $exception, $request)
+    {
+        $errors = $exception->validator->errors()->getMessages();
+
+
+
+        return $this->errorResponse($errors, 422);
     }
 }
